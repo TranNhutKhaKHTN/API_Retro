0458d301d732ce39ae1a74936b679d20fb6f7616
TranNhutKhaKHTN <trannhutkha123456@gmail.com>
2020-11-10 23:42:48 +0700

    fix header policy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
